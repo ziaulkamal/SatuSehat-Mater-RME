3ee63b1805afe8b4a6d811f34ea6efe94f1bc346
Ziaul Kamal <codepad@usa.com>
2024-07-12 17:52:45 +0700

    update

--- app/Services/OAuth2Client.php
+++ app/Services/OAuth2Client.php
@@ -16,54 +16,47 @@ class OAuth2Client {
             $statusCode = 500;
             $response   = 'Const ID Tidak Ditemukan';
             return [ $response, $statusCode ];
-        }
-        $userTokenCredential = UserTokenCredential::where('const_users', $const_users)
-            ->orderBy('created_at', 'desc')
-            ->first();
+        }else {
+            $userTokenCredential = UserTokenCredential::where('const_users', $const_users)
+                ->orderBy('created_at', 'desc')
+                ->first();
 
-        if (!$userTokenCredential || $userTokenCredential->created_at->diffInMinutes($now) > 50 || $userTokenCredential->total_hit > 100) {
-            $client = new Client();
+            if (!$userTokenCredential || $userTokenCredential->created_at->diffInMinutes($now) > 50 || $userTokenCredential->total_hit > 100) {
+                $client = new Client();
 
-            $headers = [
-                'Content-Type' => 'application/x-www-form-urlencoded',
-            ];
-            $options = [
-                'form_params' => [
-                    'client_id' => $data->client_id,
-                    'client_secret' => $data->secret_id,
-                    'grant_type' => 'client_credentials'
-                ],
-                'headers' => $headers
-            ];
+                $headers = [
+                    'Content-Type' => 'application/x-www-form-urlencoded',
+                ];
+                $options = [
+                    'form_params' => [
+                        'client_id' => $data->client_id,
+                        'client_secret' => $data->secret_id,
+                    ],
+                ];
 
-            // Create session
-            $url = 'https://api-satusehat.kemkes.go.id/oauth2/v1/accesstoken';
-        } else {
-            $response   = $userTokenCredential->token;
-            $statusCode = 200;
-            $userTokenCredential->increment('total_hit');
-            return [ $response, $statusCode ];
-        }
+                // Create session
+                $url = 'https://api-satusehat.kemkes.go.id/oauth2/v1/accesstoken?grant_type=client_credentials';
+                $request = new Request('POST', $url, $headers);
 
-        try {
-            $res = $client->post($url, $options);
-            $contents = json_decode($res->getBody()->getContents());
+                $res = $client->sendAsync($request, $options)->wait();
+                $contents = json_decode($res->getBody()->getContents());
 
-            $statusCode = $res->getStatusCode();
-            $response   = $contents->access_token;
+                $statusCode = $res->getStatusCode();
+                $response   = $contents->access_token;
 
-            UserTokenCredential::create([
-                'const_users' => $const_users,
-                'token'       => $response,
-                'total_hit'   => 1
-            ]);
+                UserTokenCredential::create([
+                    'const_users' => $const_users,
+                    'token'       => $response,
+                    'total_hit'   => 1
+                ]);
 
-            return [ $response, $statusCode ];
-        } catch (\Exception $e) {
-            $statusCode = 500;
-            $response   = 'Const ID Tidak Ditemukan';
-            return [ $response, $statusCode ];
+                return [ $response, $statusCode ];
+            } else {
+                $response   = $userTokenCredential->token;
+                $statusCode = 200;
+                $userTokenCredential->increment('total_hit');
+                return [ $response, $statusCode ];
+            }
         }
     }
 }
-
