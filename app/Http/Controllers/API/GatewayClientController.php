d0bd292761db606883459b952caa5e31a8ebc891
Ziaul Kamal <codepad@usa.com>
2024-07-13 22:30:18 +0700

    update

--- app/Http/Controllers/API/GatewayClientController.php
+++ app/Http/Controllers/API/GatewayClientController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\API\BaseController;
 use App\Http\Controllers\Controller;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Psr7\Request;
 
 class GatewayClientController extends BaseController
@@ -15,22 +16,30 @@ class GatewayClientController extends BaseController
         $headers = [
             'Content-Type' => 'application/x-www-form-urlencoded',
         ];
-        $options = [
-            'form_params' => [
-                'client_id' => '37ISDRZj9EBUf9WPujky7HY2S6gKADpnLKqzuDdf63Vrnphc',
-                'client_secret' => 'QuqhjY8aK6COUnItyAl5EJKdyOTiE5pADg44GCtxtxvrefmdOU5wnd6S2WSYhzt3',
-            ],
-        ];
+        $body = http_build_query([
+            'client_id' => '37ISDRZj9EBUf9WPujky7HY2S6gKADpnLKqzuDdf63Vrnphc',
+            'client_secret' => 'QuqhjY8aK6COUnItyAl5EJKdyOTiE5pADg44GCtxtxvrefmdOU5wnd6S2WSYhzt3',
+        ]);
 
-        // Create session
-        $url        = 'https://api-satusehat.kemkes.go.id/oauth2/v1/accesstoken?grant_type=client_credentials';
-        $request    = new Request('POST', $url, $headers);
+        $url = 'https://api-satusehat.kemkes.go.id/oauth2/v1/accesstoken?grant_type=client_credentials';
+        $request = new Request('POST', $url, $headers, $body);
 
-        $res        = $client->sendAsync($request, $options)->wait();
-        $contents   = json_decode($res->getBody()->getContents());
+        try {
+            $res = $client->sendAsync($request)->wait();
+            $contents = json_decode($res->getBody()->getContents());
 
-        $statusCode = $res->getStatusCode();
-        $response   = $contents->access_token;
-        dd($contents);
+            $statusCode = $res->getStatusCode();
+            $response = $contents->access_token;
+            dd($contents);
+        } catch (RequestException $e) {
+            if ($e->hasResponse()) {
+                $response = $e->getResponse();
+                $statusCode = $response->getStatusCode();
+                $body = $response->getBody()->getContents();
+                dd($statusCode, $body);
+            } else {
+                dd($e->getMessage());
+            }
+        }
    }
 }
